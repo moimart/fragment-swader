d41c43052d1b4eb2f3d60856007ba1a26c7fcbcc
Moises Martinez <moimart@gmail.com>
2018-09-04 16:34:38 +0200

    Improving library setup

--- webpack.config.js
+++ webpack.config.js
@@ -28,4 +28,33 @@ module.exports = [{
             'node_modules'
         ]
     }
+},
+{
+    target: 'node',
+    mode: "production",
+    externals: [
+        /^[a-z\-0-9]+$/ // Ignore node_modules folder
+    ],
+    output: {
+        filename: 'swader.js', // output file
+        path: __dirname,
+        libraryTarget: "commonjs"
+    },
+    entry: './swader/index.ts',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            options: {
+                configFile: 'lib.tsconfig.json'
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        modules: [
+            path.join(__dirname, "/node_modules"),
+            'node_modules'
+        ]
+    }
 }]
